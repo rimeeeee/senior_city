335e386f32cc16c97ac178dc5c14fac183062bb8
Eden <lje970722@gmail.com>
2025-05-17 23:20:14 +0900

    trial

--- app.py
+++ app.py
@@ -913,4 +913,5 @@ def district_features():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5050)))
+    
